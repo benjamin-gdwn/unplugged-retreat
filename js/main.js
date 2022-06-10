c1b06d9fc384864ddda2579721777a3187c7850f
Ben_G <ben.goodwin.work1@gmail.com>
2022-06-10 10:22:36 +0100

    Initial commit gh pages

---
+++ js/main.js
@@ -0,0 +1,15 @@
+// var name = "Ben";
+// var favouriteSnack = "Pizza";
+// console.log(
+//   "Hi, my name is " +
+//     name +
+//     " and i wish i had a " +
+//     favouriteSnack +
+//     " right now!"
+// );
+
+// console.log(name);
+// console.log('Hello world');
+
+
+var question = prompt('What is your name?');
